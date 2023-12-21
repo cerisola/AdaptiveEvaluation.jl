5ff543c7ca7fb707aed53a6595d04096b98a6020
Documenter.jl <documenter@juliadocs.github.io>
2023-12-21 16:50:46 +0000

    build based on 5772658

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AdaptiveEvaluation","category":"page"},{"location":"#AdaptiveEvaluation","page":"Home","title":"AdaptiveEvaluation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AdaptiveEvaluation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AdaptiveEvaluation]","category":"page"}]
+}
